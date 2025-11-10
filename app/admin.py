25578d813bbccaf5f5b0d232e52732419843ace8
aarlozz <kiransilwal108@gmail.com>
2025-11-10 21:31:50 +0545

    Added models and started schema

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Profile, Event, Task
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Event)
+admin.site.register(Task)
---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#this says that the SignupForm is inheroted from the UserCreationForm
+class SignupForm(UserCreationForm):
+    name=forms.CharField(required=True)
+    email= forms.EmailField(required=True)
+
+    class Meta:
+        model=User # This tells that the model is linked with inbuilt User model
+        fields=['username','email', 'password1','password2']
+
+#form ma widget use garera forntend use garney

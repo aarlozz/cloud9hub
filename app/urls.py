79812a50670634259ba5915f47fa4ee9d5625c37
aarlozz <kiransilwal108@gmail.com>
2025-11-11 21:00:49 +0545

    signup form added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('signup/', views.signup_view, name='signup'),
     
     
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+def signup_view(request):
+    if request.method=='POST':
+        form= UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user=form.save()
+            login(user,request)
+            return redirect ('dashboard')
+    else:
+            form=UserCreationForm()
+
+    return render(request, 'signupform.html', {'form': form})        
 
 # Create your views here.
+
